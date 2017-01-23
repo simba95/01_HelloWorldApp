2665032b0feac7c429b1d181e10e110dfd876873
bad4iz <bad4iz@gmail.com>
2017-01-23 20:07:14 +0400

    bn bnhbhj

--- app/app.component.js
+++ app/app.component.js
@@ -19,7 +19,7 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>My First Angular App</h1>' // HTML разметка определяющая представление текущего компонента
+            template: '<h1>My First kjnkjkjlkjlkjjknnjkkjnkjnk App</h1>' // HTML разметка определяющая представление текущего компонента
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
